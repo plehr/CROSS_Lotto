857995161c8dcb917bb7d1e725c6c74a1175f23b
PLehr.de <spam@plehr.de>
2021-03-26 16:05:03 +0100

    added logic

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,31 @@
 document.addEventListener('deviceready', init, false);
 
+var arr;
+var maxNum=6;
+
 function init() {
-   //TODO
+    arr = [];
+    for (let index = 1; index <= 49; index++)
+        arr.push(index)
+    document.getElementById("btn_play").addEventListener("click", function () {
+
+
+        document.getElementById("count").innerHTML = "";
+        for (let i = 0; i < maxNum; i++) {
+            var num;
+            do {
+                num = getRandNum(1, 49);
+            } while (!checkNum(num))
+            document.getElementById("count").innerHTML += num + "";
+            if (i < maxNum -1)
+                document.getElementById("count").innerHTML += ",";
+        }
+
+    })
+}
+function getRandNum(from, to) {
+    return Math.floor(Math.random(from) * Math.floor(to))
+}
+function checkNum(num) {
+    return arr.includes(num);
 }
